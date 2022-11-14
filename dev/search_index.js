8d0fc45a3be1da681b005a7ab92305fed8a1b826
Documenter.jl <documenter@juliadocs.github.io>
2022-11-14 15:55:05 +0000

    build based on 83702ec

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Limace","category":"page"},{"location":"#Limace","page":"Home","title":"Limace","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Limace.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Limace]","category":"page"}]
+}
