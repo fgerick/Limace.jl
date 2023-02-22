10f50cd45e22e26be3192e2987b8b5283c31ea5e
Felix <felixgerick@gmail.com>
2023-02-22 17:01:37 +0100

    add rquad shell

--- src/discrete/quad.jl
+++ src/discrete/quad.jl
@@ -8,6 +8,16 @@ function rquad(nr)
 	return _r, _wr
 end
 
+# ∫ₐᵇ f(r) r² dr
+function rquad(nr,a,b) 
+	# _r, _wr = gaussjacobi(nr,0.0,2.0) 
+	_r, _wr = gausslegendre(nr)
+	_r = ((b-a)*_r .+ b .+ a)/2
+	_wr *= (b-a)/2
+	# _wr /= 8
+	return _r, _wr
+end
+
 
 function ∫dr(f::F,r::Vector{T},wr::Vector{T})::Complex{T} where {F,T}
 	out = zero(ComplexF64)
