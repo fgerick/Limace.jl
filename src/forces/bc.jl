5b6022cb8b7030bd2a36ecda9eb063a54d2011e6
Felix <felixgerick@gmail.com>
2024-07-22 18:17:14 +0200

    add bc zero row

--- src/forces/bc.jl
+++ src/forces/bc.jl
@@ -121,3 +121,38 @@ $(TYPEDSIGNATURES)
 
     return nothing
 end
+
+@inline function zero_boundarycondition_row!(A, b::TB, ::Type{T}=Float64) where {TB<:Basis,T<:Number}
+
+    lmn2k_p = lmn2k_p_dict(b)
+    lmn2k_t = lmn2k_t_dict(b)
+    _np = np(b)
+
+    #m == m2 and only l==l2 needs to be considered.
+    for l in 1:lpmax(b)
+        for m in intersect(b.m, -l:l)
+            if (b.BC == NoBC()) 
+                bcf = bcs_p(b) #tuple of evaluation functions
+                npmax = last(nrange_p(b,l))
+                for (i,f) in enumerate(bcf)
+                    A[lmn2k_p[(l,m,npmax-i+1)], :] = 0
+                end
+            end
+        end
+    end
+
+    for l in 1:ltmax(b)
+        for m in intersect(b.m, -l:l)
+            if (b.BC == NoBC())
+                bcf = bcs_t(b) #tuple of evaluation functions
+                ntmax = last(nrange_t(b,l))
+                for (i,f) in enumerate(bcf)
+                    A[lmn2k_t[(l,m,ntmax-i+1)] + _np, :] = 0
+                end
+            end
+        end
+    end
+
+
+    return nothing
+end
