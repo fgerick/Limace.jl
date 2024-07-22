abb0e7b96410f70d90c337fc95c48969a1194e2e
Felix <felixgerick@gmail.com>
2024-07-22 11:30:54 +0200

    add mutating bc function

--- src/forces/bc.jl
+++ src/forces/bc.jl
@@ -40,3 +40,45 @@ $(TYPEDSIGNATURES)
 
     return sparse(is, js, aijs, nbasis, nbasis)
 end
+
+"""
+$(TYPEDSIGNATURES)
+
+"""
+@inline function boundarycondition!(A, b::TB, ::Type{T}=Float64) where {TB<:Basis,T<:Number}
+
+    lmn2k_p = lmn2k_p_dict(b)
+    lmn2k_t = lmn2k_t_dict(b)
+    _np = np(b)
+    nbasis = length(b)
+
+    #m == m2 and only l==l2 needs to be considered.
+    for l in 1:lpmax(b)
+        for m in intersect(b.m, -l:l)
+            if (b.BC == NoBC()) 
+                bcf = bcs_p(b) #tuple of evaluation functions
+                npmax = last(nrange_p(b,l))
+                for (i,f) in enumerate(bcf), n2 in nrange_p(b,l)
+                    bij = f(l,n2)
+                    A[lmn2k_p[(l,m,npmax-i+1)], lmn2k_p[(l,m,n2)]] = bij
+                end
+            end
+        end
+    end
+
+    for l in 1:ltmax(b)
+        for m in intersect(b.m, -l:l)
+            if (b.BC == NoBC())
+                bcf = bcs_t(b) #tuple of evaluation functions
+                ntmax = last(nrange_t(b,l))
+                for (i,f) in enumerate(bcf), n2 in nrange_t(b,l)
+                    bij = f(l,n2)
+                    A[lmn2k_t[(l,m,ntmax-i+1)] + _np, lmn2k_t[(l,m,n2)] + _np] = bij
+                end
+            end
+        end
+    end
+
+
+    return nothing
+end
