31b0b4ddf3eddf412784c63861ed791ec82b2cf2
Felix Gerick <felixgerick@gmail.com>
2025-10-23 11:55:54 +0100

    fix specializations lck

--- src/forces/specializations.jl
+++ src/forces/specializations.jl
@@ -2,7 +2,7 @@ const Tunion = Union{Insulating, Inviscid, Viscous, ThinWall} #all bases that ca
 
 # only assemble cross terms that are neigboring up to max(ni-(n0+1)-l0,first(njs_all)):min(ni+n0+1+l0,last(njs_all))
 
-@inline function _crossterm!(bbi::Basis{Ti,V}, U0::BasisElement{Basis{T0,V},PT,T}, bbj::Basis{Tj,V}, is, js, aijs, i0, j0,
+@inline function _crossterm!(bbi::Basis{Ti,V}, U0::BasisElement{Basis{T0,V},PT,T}, bbj::Basis{Tj,V}, is, js, aijs, lck, i0, j0,
     li, mi, lj, mj, rwrs, lmn2k_bi, lmn2k_bj, nrangefi, nrangefj, indf, EA; kwargs...) where {Ti<:Tunion, Tj<:Tunion, T0<:Tunion, PT<:Helmholtz,T, V<:Volume}
     l0,m0,n0 = U0.lmn
     for ni in nrangefi(bbi, li)
@@ -12,13 +12,13 @@ const Tunion = Union{Insulating, Inviscid, Viscous, ThinWall} #all bases that ca
             lmni = (li, mi, ni)
             lmnj = (lj, mj, nj)
             aij = indf(Basis{T0,V}, Basis{Tj,V}, Basis{Ti,V}, bbi.V, U0.lmn, lmnj, lmni, r, wr; kwargs...)*EA
-            appendit!(is, js, aijs, lmn2k_bi[lmni] + i0, lmn2k_bj[lmnj] + j0, aij*U0.factor)
+            appendit!(is, js, aijs, lck, lmn2k_bi[lmni] + i0, lmn2k_bj[lmnj] + j0, aij*U0.factor)
         end
     end
     return nothing
 end
 
-@inline function _crossterm!(bbi::Basis{Ti,V}, buj::Basis{Tj,V}, B0::BasisElement{Basis{T0,V},PT,T}, is, js, aijs, i0, j0,
+@inline function _crossterm!(bbi::Basis{Ti,V}, buj::Basis{Tj,V}, B0::BasisElement{Basis{T0,V},PT,T}, is, js, aijs, lck, i0, j0,
     li, mi, lj, mj, rwrs, lmn2k_bi, lmn2k_uj, nrangefi, nrangefj, indf, EA; kwargs...) where {Ti<:Tunion, Tj<:Tunion, T0<:Tunion, PT<:Helmholtz,T, V<:Volume}
     l0,m0,n0 = B0.lmn
     for ni in nrangefi(bbi, li)
@@ -29,14 +29,14 @@ end
             lmni = (li, mi, ni)
             lmnj = (lj, mj, nj)
             aij = indf(Basis{Tj,V}, Basis{T0,V}, Basis{Ti,V}, bbi.V, lmnj, B0.lmn, lmni, r, wr; kwargs...)*EA
-            appendit!(is, js, aijs, lmn2k_bi[lmni] + i0, lmn2k_uj[lmnj] + j0, aij*B0.factor)
+            appendit!(is, js, aijs, lck, lmn2k_bi[lmni] + i0, lmn2k_uj[lmnj] + j0, aij*B0.factor)
         end
     end
     return nothing
 end
 
 
-@inline function _crossterm_m_adamgaunt!(bbi::Basis{Ti,V}, buj::Basis{Tj,V}, B0::BasisElement{Basis{T0,V},PT,T}, is, js, aijs, i0, j0,
+@inline function _crossterm_m_adamgaunt!(bbi::Basis{Ti,V}, buj::Basis{Tj,V}, B0::BasisElement{Basis{T0,V},PT,T}, is, js, aijs, lck, i0, j0,
     li, ni, lj, rwrs, lmn2k_bi, lmn2k_uj, nrangefj, lptmax, indf; kwargs...) where {Ti<:Tunion, Tj<:Tunion, T0<:Tunion, PT<:Helmholtz,T, V<:Volume}
     l0,m0,n0 = B0.lmn
     njs_all = nrangefj(buj,lj)
@@ -60,14 +60,14 @@ end
                 iA +=1
                 lmni = (li, mi, ni)
                 lmnj = (lj, mj, nj)
-                appendit!(is, js, aijs, lmn2k_bi[lmni]+i0, lmn2k_uj[lmnj]+j0, aij*A*B0.factor)
+                appendit!(is, js, aijs, lck, lmn2k_bi[lmni]+i0, lmn2k_uj[lmnj]+j0, aij*A*B0.factor)
             end
         end
     end
     return nothing
 end
 
-@inline function _crossterm_m_elsasser!(bbi::Basis{Ti,V}, buj::Basis{Tj,V}, B0::BasisElement{Basis{T0,V},PT,T}, is, js, aijs, i0, j0,
+@inline function _crossterm_m_elsasser!(bbi::Basis{Ti,V}, buj::Basis{Tj,V}, B0::BasisElement{Basis{T0,V},PT,T}, is, js, aijs, lck, i0, j0,
     li, ni, lj, rwrs, lmn2k_bi, lmn2k_uj, nrangefj, lptmax, indf; kwargs...) where {Ti<:Tunion, Tj<:Tunion, T0<:Tunion, PT<:Helmholtz,T, V<:Volume}
     l0,m0,n0 = B0.lmn
     njs_all = nrangefj(buj,lj)
@@ -91,7 +91,7 @@ end
                 iE+=1
                 lmni = (li, mi, ni)
                 lmnj = (lj, mj, nj)
-                appendit!(is, js, aijs, lmn2k_bi[lmni]+i0, lmn2k_uj[lmnj]+j0, aij*E*B0.factor)
+                appendit!(is, js, aijs, lck, lmn2k_bi[lmni]+i0, lmn2k_uj[lmnj]+j0, aij*E*B0.factor)
             end
         end
     end
@@ -100,7 +100,7 @@ end
 
 
 
-@inline function _crossterm_m_adamgaunt!(bbi::Basis{Ti,V}, U0::BasisElement{Basis{T0,V},PT,T}, buj::Basis{Tj,V}, is, js, aijs, i0, j0,
+@inline function _crossterm_m_adamgaunt!(bbi::Basis{Ti,V}, U0::BasisElement{Basis{T0,V},PT,T}, buj::Basis{Tj,V}, is, js, aijs, lck, i0, j0,
     li, ni, lj, rwrs, lmn2k_bi, lmn2k_uj, nrangefj, lptmax, indf; kwargs...) where {Ti<:Tunion, Tj<:Tunion, T0<:Tunion, PT<:Helmholtz,T, V<:Volume}
     l0,m0,n0 = U0.lmn
     njs_all = nrangefj(buj,lj)
@@ -124,14 +124,14 @@ end
                 iA +=1
                 lmni = (li, mi, ni)
                 lmnj = (lj, mj, nj)
-                appendit!(is, js, aijs, lmn2k_bi[lmni]+i0, lmn2k_uj[lmnj]+j0, aij*A*U0.factor)
+                appendit!(is, js, aijs, lck, lmn2k_bi[lmni]+i0, lmn2k_uj[lmnj]+j0, aij*A*U0.factor)
             end
         end
     end
     return nothing
 end
 
-@inline function _crossterm_m_elsasser!(bbi::Basis{Ti,V}, U0::BasisElement{Basis{T0,V},PT,T}, buj::Basis{Tj,V}, is, js, aijs, i0, j0,
+@inline function _crossterm_m_elsasser!(bbi::Basis{Ti,V}, U0::BasisElement{Basis{T0,V},PT,T}, buj::Basis{Tj,V}, is, js, aijs, lck, i0, j0,
     li, ni, lj, rwrs, lmn2k_bi, lmn2k_uj, nrangefj, lptmax, indf; kwargs...) where {Ti<:Tunion, Tj<:Tunion, T0<:Tunion, PT<:Helmholtz,T, V<:Volume}
     l0,m0,n0 = U0.lmn
     njs_all = nrangefj(buj,lj)
@@ -155,7 +155,7 @@ end
                 iE+=1
                 lmni = (li, mi, ni)
                 lmnj = (lj, mj, nj)
-                appendit!(is, js, aijs, lmn2k_bi[lmni]+i0, lmn2k_uj[lmnj]+j0, aij*E*U0.factor)
+                appendit!(is, js, aijs, lck, lmn2k_bi[lmni]+i0, lmn2k_uj[lmnj]+j0, aij*E*U0.factor)
             end
         end
     end
