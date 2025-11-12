77476f871d4231572ff4846ad51496ce6af9da25
Felix Gerick <felixgerick@gmail.com>
2025-11-12 15:08:32 +0000

    appendit for one index

--- src/Utils.jl
+++ src/Utils.jl
@@ -11,11 +11,30 @@ export appendit!
         end
     end
 end
+
 @inline function appendit!(is::Vector{Int}, js::Vector{Int}, aijs::Union{Vector{T},Vector{Complex{T}}}, lck::ReentrantLock, i::Int, j::Int, aij::T; thresh=sqrt(eps())) where T
     if (abs(aij) > thresh)
         lock(lck) do 
             push!(is, i)
             push!(js, j)
+            push!(aijs, aij)
+        end
+    end
+end
+
+@inline function appendit!(is, aijs, lck::ReentrantLock, i, aij; thresh=sqrt(eps()))
+    if !isnothing(aij) && (abs(aij) > thresh)
+        lock(lck) do
+            push!(is, i)
+            push!(aijs, aij)
+        end
+    end
+end
+
+@inline function appendit!(is::Vector{Int}, aijs::Union{Vector{T},Vector{Complex{T}}}, lck::ReentrantLock, i::Int, aij::T; thresh=sqrt(eps())) where T
+    if (abs(aij) > thresh)
+        lock(lck) do 
+            push!(is, i)
             push!(aijs, aij)
         end
     end
