eccfe3d1acb6a5e36fa7033df08a9a5fc1964820
Felix Gerick <felixgerick@gmail.com>
2026-08-31 17:18:19 +0100

    let inertial, coriolis and diffusion take two different bases

--- src/bases/ThinWall.jl
+++ src/bases/ThinWall.jl
@@ -11,9 +11,9 @@ using ..Utils
 using ..Poly
 
 using ..Bases: nrange_p, nrange_t, nrange_p_bc, nrange_t_bc, np, nt, t, s, bcs_p, bcs_t, lmn_p, lmn_t, lmn2k_p_dict, lmn2k_t_dict, lpmax, ltmax, Sphere
-import ..Bases: lpmax, ltmax, lmn_t, lmn_p, _nrange_p, _nrange_t, np, nt, t, s, nrange_p_bc, nrange_t_bc, bcs_p, bcs_t
+import ..Bases: lpmax, ltmax, lmn_t, lmn_p, _nrange_p, _nrange_t, np, nt, t, s, nrange_p_bc, nrange_t_bc, bcs_p, bcs_t, _lmn2cdeg_p, _lmn2cdeg_t
 
-import ..Limace: inertial, _inertial_ss, _inertial_tt
+import ..Limace: inertial, _inertial_ss, _inertial_tt, _diffusion_ss, _diffusion_tt
 import ..Quadrature: rquad
 import ..Limace: inertial, diffusion
 
@@ -21,12 +21,13 @@ export ThinWall
 
 struct ThinWall{q,h}; end
 
+#based on appendix of Guervilly et al. (10.1103/PhysRevE.88.053010)
+
 function ThinWall(N; q=1.0, h = 0.0, kwargs...)
     params=Dict(:q =>q, :h => h)
     return Basis{ThinWall{q,h},Sphere}(;N, V=Sphere(), BC=ThinWallBC{q,h}(), params,  kwargs...)
 end
 
-#based on appendix of 10.1103/PhysRevE.88.053010
 function s(::Type{Basis{ThinWall{q,h},Sphere}}, V::Sphere, l,m,n,r) where {q,h}
     fac = 1/(sqrt(2l*(1 + l)*(-3 + 2*l + 4*n)*(-1 + 2*l + 4*n)*(1 + 2*l + 4*n))) # ∫s⋅s dV ≠ 1 for h!=0.
     coeff1 = (-3 + 2*l + 4*n)*(1 + h*(l + 2*l*(-1 + n)*q + (1 - 3*n + 2*n^2)*q))
@@ -42,11 +43,68 @@ function t(::Type{Basis{ThinWall{q,h},Sphere}}, V::Sphere, l,m,n,r) where {q,h}
     return fac*r^l*(jacobi(n,0,l+1/2,2r^2-1) + coeff2/coeff1*jacobi(n-1,0,l+1/2,2r^2-1))
 end
 
+#based on Roberts et al. (2010)
+
+# function ThinWall(N; c=0.0, c1 = 0.0, kwargs...)
+#     params=Dict(:c =>c, :c1 => c1)
+#     return Basis{ThinWall{c,c1},Sphere}(;N, V=Sphere(), BC=ThinWallBC{c,c1}(), params,  kwargs...)
+# end
+
+# function s(::Type{Basis{ThinWall{c,c1},Sphere}}, V::Sphere, l,m,n,r) where {c,c1}
+#     fac = 1/(sqrt(2l*(1 + l)*(-3 + 2*l + 4*n)*(-1 + 2*l + 4*n)*(1 + 2*l + 4*n))) # ∫s⋅s dV ≠ 1 for h!=0.
+#     coeff1 = (-3 + 2*l + 4*n)*(2 + 2*c1*l + c*(2 + c1*l)*(-1 + n)*(-1 + 2*l + 2*n)) 
+#     coeff2 = -(-1 + 2*l + 4*n)*(4 + 4*c1*l + c*(2 + c1*l)*(3 - 2*n + 4*n^2 + l*(-2 + 4*n)))
+#     coeff3 = (1 + 2*l + 4*n)*(2 + 2*c*n*(1 + 2*l + 2*n) + c1*l*(2 + c*n*(1 + 2*l + 2*n)))
+#     return fac*r^l*(coeff1*jacobi(n,0,l+1/2,2r^2-1) + coeff2*jacobi(n-1,0,l+1/2,2r^2-1) + coeff3*jacobi(n-2,0,l+1/2,2r^2-1))
+# end
+
+# function t(::Type{Basis{ThinWall{c,c1},Sphere}}, V::Sphere, l,m,n,r) where {c,c1}
+#     fac = 1/sqrt(l*(1 + l)*(1/(-1 + 2*l + 4*n) + 1/(3 + 2*l + 4*n))) # ∫t⋅t dV ≠ 1 for h!=0.
+#     coeff1 = (1 + c*(l + n)*(-1 + 2*n))
+#     coeff2 = -(1 + c*(1 + l + n)*(1 + 2*n))
+#     return fac*r^l*(jacobi(n,0,l+1/2,2r^2-1) + coeff2/coeff1*jacobi(n-1,0,l+1/2,2r^2-1))
+# end
+
 @inline _nrange_p(b::Basis{ThinWall{q,h},Sphere},l) where {q,h} = 1:((b.N-l+1)÷2)
 @inline _nrange_t(b::Basis{ThinWall{q,h},Sphere},l) where {q,h} = 1:((b.N-l)÷2)
 
 lpmax(b::Basis{ThinWall{q,h},Sphere}) where {q,h} = b.N
 ltmax(b::Basis{ThinWall{q,h},Sphere}) where {q,h} = b.N
+
+_lmn2cdeg_p(b::Basis{ThinWall{q,h},Sphere}, l,m,n) where {q,h}  = l+2n-1
+_lmn2cdeg_t(b::Basis{ThinWall{q,h},Sphere}, l,m,n) where {q,h} = l+2n
+
+
+function diffusion(b::Basis{ThinWall{q,h}, Sphere}; η::T=1.0, threads=false, external=true) where {q,h,T}
+    lmnp = lmn_p(b)
+    lmnt = lmn_t(b)
+    r, wr = rquad(b.N+5,b.V)
+
+    diff_s = [_diffusion_ss(b,b, (l,m,n), (l,m,n), r, wr)*η for (l,m,n) in lmnp]
+    diff_t = [_diffusion_tt(b,b, (l,m,n), (l,m,n), r, wr)*η for (l,m,n) in lmnt]
+    return spdiagm(vcat(diff_s,diff_t))
+end
+
+function inertial(b::Basis{ThinWall{q,h}, Sphere}; threads=false, external=true) where {q,h}
+    T = typeof(b.V.r1)
+    lmnp = lmn_p(b)
+    lmnt = lmn_t(b)
+
+    r, wr = rquad(b.N+5,b.V)
+
+
+    inertial_s = [T(_inertial_ss(b,b, (l,m,n), (l,m,n), r, wr)) for (l,m,n) in lmnp]
+    inertial_s2 = [(n-1 > 0 ? T(_inertial_ss(b,b, (l,m,n), (l,m,n-1),r,wr)) : zero(T)) for (l,m,n) in lmnp[2:end]]
+    inertial_t = [T(_inertial_tt(b,b, (l,m,n), (l,m,n), r, wr)) for (l,m,n) in lmnt]
+    inertial_t2 = [(n-1 > 0 ? T(_inertial_tt(b,b, (l,m,n), (l,m,n-1),r,wr)) : zero(T)) for (l,m,n) in lmnt[2:end]]
+
+    d = vcat(inertial_s,inertial_t)
+    d2 = [inertial_s2;0.0; inertial_t2]
+    return complex(sparse(SymTridiagonal(d, d2)))
+end 
+
+
+
 
 
 end
--- src/forces/inertial.jl
+++ src/forces/inertial.jl
@@ -38,33 +38,35 @@ end
 $(TYPEDSIGNATURES)
 
 """
-function _inertial_ss(b::T, lmna, lmnb, r,wr; external=false) where T<:Basis
+function _inertial_ss(bi::Ti, bj::Tj, lmna, lmnb, r,wr; external=false) where {Ti<:Basis, Tj<:Basis}
     la,ma,na = lmna
     lb,mb,nb = lmnb
 
-    @inline _sa = r->s(T,b.V,la,ma,na,r)
-    @inline _sb = r->s(T,b.V,lb,mb,nb,r)
+    @inline _sa = r->s(Ti,bi.V,la,ma,na,r)
+    @inline _sb = r->s(Tj,bj.V,lb,mb,nb,r)
 
     @inline f = r-> inners(_sa,_sb, la, r)
 
     aij = ∫dr(f,r,wr)
 
     if external && (la==lb) && (ma==mb)
-        aij += _sa(b.V.r1)*_sb(b.V.r1)*la^2*(la+1)
+        aij += _sa(bi.V.r1)*_sb(bj.V.r1)*la^2*(la+1)
     end
     return aij
 end
+
+_inertial_ss(b::T, lmna, lmnb, r, wr; external=false) where T<:Basis = _inertial_ss(b, b, lmna, lmnb, r,wr; external)
 
 """
 $(TYPEDSIGNATURES)
 
 """
-function _inertial_tt(b::T, lmna, lmnb, r,wr) where T<:Basis
+function _inertial_tt(bi::Ti, bj::Tj, lmna, lmnb, r,wr) where {Ti<:Basis, Tj<:Basis}
     la,ma,na = lmna
     lb,mb,nb = lmnb
 
-    @inline _ta = r->t(T,b.V,la,ma,na,r)
-    @inline _tb = r->t(T,b.V,lb,mb,nb,r)
+    @inline _ta = r->t(Ti,bi.V,la,ma,na,r)
+    @inline _tb = r->t(Tj,bj.V,lb,mb,nb,r)
 
     @inline f = r-> innert(_ta,_tb, la, r)
 
@@ -72,72 +74,118 @@ function _inertial_tt(b::T, lmna, lmnb, r,wr) where T<:Basis
     return aij
 end
 
+_inertial_tt(b::T, lmna, lmnb, r, wr) where T<:Basis = _inertial_tt(b, b, lmna, lmnb, r,wr)
 
-function _inertial(::Val{false}, b::Basis; external=false)
 
-    T = typeof(b.V.r1)
+# function _inertial(::Val{false}, b::Basis; external=false)
+
+#     T = typeof(b.V.r1)
+#     is, js, aijs = Int[], Int[], complex(T)[]
+#     lmn2k_p = lmn2k_p_dict(b)
+#     lmn2k_t = lmn2k_t_dict(b)
+#     _np = np(b)
+#     r, wr = rquad(b.N + 5, b.V)
+#     nu = length(b)
+#     lck = ReentrantLock()
+
+#     #m == m2 and only l==l2 needs to be considered.
+#     for l in 1:lpmax(b)
+#         for m in intersect(b.m, -l:l)
+#             for n in nrange_p_bc(b,l), n2 in nrange_p(b,l)
+#                 aij = _inertial_ss(b, (l,m,n), (l,m,n2), r,wr; external)
+#                 appendit!(is, js, aijs, lck, lmn2k_p[(l,m,n)], lmn2k_p[(l,m,n2)], aij)
+#             end
+#         end
+#     end
+
+#     for l in 1:ltmax(b)
+#         for m in intersect(b.m, -l:l)
+#             for n in nrange_t_bc(b,l), n2 in nrange_t(b,l)
+#                 aij = _inertial_tt(b, (l,m,n), (l,m,n2), r,wr)
+#                 appendit!(is, js, aijs, lck, lmn2k_t[(l,m,n)] + _np, lmn2k_t[(l,m,n2)] + _np, aij)
+#             end
+#         end
+#     end
+
+
+#     return sparse(is, js, aijs, nu, nu)
+# end
+
+function _inertial(::Val{false}, bi::Basis, bj::Basis; external=false)
+
+    T = promote_type(typeof(bi.V.r1),typeof(bj.V.r1))
     is, js, aijs = Int[], Int[], complex(T)[]
-    lmn2k_p = lmn2k_p_dict(b)
-    lmn2k_t = lmn2k_t_dict(b)
-    _np = np(b)
-    r, wr = rquad(b.N + 5, b.V)
-    nu = length(b)
+    lmn2k_pi = lmn2k_p_dict(bi)
+    lmn2k_ti = lmn2k_t_dict(bi)
+    lmn2k_pj = lmn2k_p_dict(bj)
+    lmn2k_tj = lmn2k_t_dict(bj)
+    _npi = np(bi)
+    _npj = np(bj)
+    r, wr = rquad(max(bi.N,bj.N) + 5, bi.V)
+    nui = length(bi)
+    nuj = length(bj)
     lck = ReentrantLock()
 
     #m == m2 and only l==l2 needs to be considered.
-    for l in 1:lpmax(b)
-        for m in intersect(b.m, -l:l)
-            for n in nrange_p_bc(b,l), n2 in nrange_p(b,l)
-                aij = _inertial_ss(b, (l,m,n), (l,m,n2), r,wr; external)
-                appendit!(is, js, aijs, lck, lmn2k_p[(l,m,n)], lmn2k_p[(l,m,n2)], aij)
+    for l in 1:lpmax(bi)
+        for m in intersect(bi.m, -l:l)
+            for n in nrange_p_bc(bi,l), n2 in nrange_p(bj,l)
+                aij = _inertial_ss(bi, bj, (l,m,n), (l,m,n2), r,wr; external)
+                appendit!(is, js, aijs, lck, lmn2k_pi[(l,m,n)], lmn2k_pj[(l,m,n2)], aij)
             end
         end
     end
 
-    for l in 1:ltmax(b)
-        for m in intersect(b.m, -l:l)
-            for n in nrange_t_bc(b,l), n2 in nrange_t(b,l)
-                aij = _inertial_tt(b, (l,m,n), (l,m,n2), r,wr)
-                appendit!(is, js, aijs, lck, lmn2k_t[(l,m,n)] + _np, lmn2k_t[(l,m,n2)] + _np, aij)
+    for l in 1:ltmax(bi)
+        for m in intersect(bi.m, -l:l)
+            for n in nrange_t_bc(bi,l), n2 in nrange_t(bj,l)
+                aij = _inertial_tt(bi, bj, (l,m,n), (l,m,n2), r,wr)
+                appendit!(is, js, aijs, lck, lmn2k_ti[(l,m,n)] + _npi, lmn2k_tj[(l,m,n2)] + _npj, aij)
             end
         end
     end
 
 
-    return sparse(is, js, aijs, nu, nu)
+    return sparse(is, js, aijs, nui, nuj)
 end
 
-function _inertial(::Val{true}, b::Basis; external=false)
+_inertial(::Val{false}, b::Basis; external=false) = _inertial(Val(false), b, b; external)
 
-    T = typeof(b.V.r1)
+function _inertial(::Val{true}, bi::Basis, bj::Basis; external=false)
+
+    T = promote_type(typeof(bi.V.r1),typeof(bj.V.r1))
     is, js, aijs = Int[], Int[], complex(T)[]
     lck = ReentrantLock()
 
-    lmn2k_p = lmn2k_p_dict(b)
-    lmn2k_t = lmn2k_t_dict(b)
-    _np = np(b)
-    r, wr = rquad(b.N + 5, b.V)
-    nu = length(b)
+    lmn2k_pi = lmn2k_p_dict(bi)
+    lmn2k_ti = lmn2k_t_dict(bi)
+    lmn2k_pj = lmn2k_p_dict(bj)
+    lmn2k_tj = lmn2k_t_dict(bj)
+    _npi = np(bi)
+    _npj = np(bj)
+    r, wr = rquad(max(bi.N,bj.N) + 5, bi.V)
+    nui = length(bi)
+    nuj = length(bj)
 
     #m == m2 and only l==l2 needs to be considered.
     @sync begin
-        for l in 1:lpmax(b)
-            for m in intersect(b.m, -l:l)
-                for n in nrange_p_bc(b,l), n2 in nrange_p(b,l)
+        for l in 1:lpmax(bi)
+            for m in intersect(bi.m, -l:l)
+                for n in nrange_p_bc(bi,l), n2 in nrange_p(bj,l)
                     Threads.@spawn begin
-                        aij = _inertial_ss(b, (l,m,n), (l,m,n2), r,wr; external)
-                        appendit!(is, js, aijs, lck, lmn2k_p[(l,m,n)], lmn2k_p[(l,m,n2)], aij)
+                        aij = _inertial_ss(bi,bj, (l,m,n), (l,m,n2), r,wr; external)
+                        appendit!(is, js, aijs, lck, lmn2k_pi[(l,m,n)], lmn2k_pj[(l,m,n2)], aij)
                     end
                 end
             end
         end
 
-        for l in 1:ltmax(b)
-            for m in intersect(b.m, -l:l)
-                for n in nrange_t_bc(b,l), n2 in nrange_t(b,l)
+        for l in 1:ltmax(bi)
+            for m in intersect(bi.m, -l:l)
+                for n in nrange_t_bc(bi,l), n2 in nrange_t(bj,l)
                     Threads.@spawn begin
-                        aij = _inertial_tt(b, (l,m,n), (l,m,n2), r,wr)
-                        appendit!(is, js, aijs, lck, lmn2k_t[(l,m,n)] + _np, lmn2k_t[(l,m,n2)] + _np, aij)
+                        aij = _inertial_tt(bi, bj, (l,m,n), (l,m,n2), r,wr)
+                        appendit!(is, js, aijs, lck, lmn2k_ti[(l,m,n)] + _npi, lmn2k_tj[(l,m,n2)] + _npj, aij)
                     end
                 end
             end
@@ -145,8 +193,10 @@ function _inertial(::Val{true}, b::Basis; external=false)
     end
 
 
-    return  sparse(vcat(is...), vcat(js...), vcat(aijs...), nu, nu)
+    return  sparse(vcat(is...), vcat(js...), vcat(aijs...), nui, nuj)
 end
+
+_inertial(::Val{true}, b::Basis; external=false) = _inertial(Val(true), b, b; external)
 
 """
 $(TYPEDSIGNATURES)
@@ -154,4 +204,6 @@ $(TYPEDSIGNATURES)
 Compute the Galerkin projection matrix of basis `b` onto itself, i.e. the inner products. 
 Also known as the mass matrix.
 """
-inertial(b::Basis; threads=false, external=false) = _inertial(Val(threads), b; external)
+inertial(b::Basis; threads=false, external=false) = inertial(b, b; threads, external)
+
+inertial(bi::Basis, bj::Basis; threads=false, external=false) = _inertial(Val(threads), bi, bj; external)
